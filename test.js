b0f4664b17721b9344fa24f2e8c9c57af5227062
Chris Gardner <ctgardner89@gmail.com>
2016-08-25 14:57:25 -0400

    challenged

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ let webdriver = require('selenium-webdriver');
 let By = webdriver.By;
 
 let d = new webdriver.Builder()
-  .forBrowser('firefox')
+  .forBrowser('chrome')
   .build();
 
 after(function(done){
@@ -34,40 +34,61 @@ describe('Social Tables Help Page', function(){
 
   // Test 1
   // note - change xdescribe to describe to get this code to run
-  xdescribe('Logo', function(){
+  describe('Logo', function(){
     it('should link to https://www.socialtables.com/', function(done){
-
-      // Insert Your Code Here
-
+      d.findElement(By.id('logo-grey')).click();
+      d.getCurrentUrl().then(currentUrl => currentUrl.toString().should.equal('http://socialtables.digihey.com/'))
+      .then(()=>done())
+      .catch(error => done(error));
     });
   });
 
   // Test 2
-  xdescribe('Searching for \'Bobby Fisher\'', function(){
+  describe('Searching for \'Bobby Fisher\'', function(){
     it('should return 0 results', function(done){
-
+      d.findElement(By.id('searchAskInput')).sendKeys('Bobby Fisher')
+      d.findElement(By.id('searchAskButton')).click()
+      .then(() => d.findElements(By.className('article')).then(elements => elements.length.should.equal(0)))
+      .then(() => done())
+      .catch(error => done(error));
       // Insert Your Code Here
-
     });
   });
 
   // Test 3
-  xdescribe('Searching for \'event\'', function(){
+  describe('Searching for \'event\'', function(){
     it('should return 10 results', function(done){
-
+      d.findElement(By.id('searchAskInput')).sendKeys('event')
+      d.findElement(By.id('searchAskButton')).click()
+      .then(() => d.findElements(By.className('article')).then(elements => elements.length.should.equal(10)))
+      .then(() => done())
+      .catch(error => done(error));
       // Insert Your Code Here
 
     })
   });
 
   // Test 4
-  xdescribe('Searching for a word under three character', function(){
+  describe('Searching for a word under three character', function(){
     it('should trigger an alert box with the text \'Search string must be at least 3 characters long\'', function(done){
-
+      d.findElement(By.id('searchAskInput')).sendKeys('is')
+      d.findElement(By.id('searchAskButton')).click()
+      d.switchTo().alert().accept()
+      .then(() => done())
+      .catch(error => done(error));
       // Insert Your Code Here
 
     });
   });
 
+  describe('Searching for \'food\'', function(){
+    it('should return 1 result', function(done){
+      d.findElement(By.id('searchAskInput')).sendKeys('food')
+      d.findElement(By.id('searchAskButton')).click()
+      .then(() => d.findElements(By.className('article')).then(elements => elements.length.should.equal(1)))
+      .then(() => done())
+      .catch(error => done(error));
+    })
+  });
 
 });
